c7cc69420a78abecbdc89d1e01141f20db9828f5
meriemrekik <rekik.meriem@gmail.com>
2024-09-30 23:48:13 +0200

    enregister formulaire ds la base de données

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +13,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request,EntityManagerInterface $entityMenager): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
+        $user = new User();
+        $form = $this->createForm(RegisterUserType::class, $user);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        return $this->render('register/index.html.twig',[
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityMenager->persist($user);
+            $entityMenager->flush();
+            // Ici, vous pouvez gérer l'envoi des données, par exemple, enregistrer l'utilisateur dans la base de données
+
+            // Redirigez vers une autre page ou affichez un message de succès
+            return $this->redirectToRoute('app_success'); // Changez 'app_success' selon vos besoins
+        }
+
+        return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView()
         ]);
     }
