ba9cf4fec054295117e058691acf3e1280737dd8
meriemrekik <rekik.meriem@gmail.com>
2024-10-01 12:10:51 +0200

    crypter le mot de passe

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,37 +17,47 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email', EmailType::class, [
-            'label' => "Votre adresse email",
-            'attr' => [
-                'placeholder' => "Entrer votre adresse email"
-            ] 
-        ])
-        ->add('password', PasswordType::class, [
-            'label' => "Votre mot de passe",
-            'attr' => [
-                'placeholder' => "Choississez votre mot de passe"
-            ] 
-        ])
-        ->add('firstname', TextType::class, [
-            'label' => "Votre prénom", 'attr' => [
-                'placeholder' => "Indiquer votre prénom"
-            ] 
-        ])
-        ->add('lastname', TextType::class, [
-            'label' => "Votre nom",
-            'attr' => [
-                'placeholder' => "Indiquer votre nom"
-            ] 
-        ])
-        ->add('submit', SubmitType::class, [
-            'label' => "Valider",
-            'attr' => [
-                'class' => "btn btn-success"
-            ] 
-
-        ]);
-        
+            ->add('email', EmailType::class, [
+                'label' => "Votre adresse email",
+                'attr' => [
+                    'placeholder' => "Entrer votre adresse email"
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => 'Votre mot de passe',
+                    'attr'  => [
+                        'placeholder' => "Confirmer votre mot de passe"
+                    ]
+                    
+                ],
+                'second_options' => [
+                    'label' => 'Répétez votre mot de passe',
+                    'attr'  => [
+                        'placeholder' => "Répétez votre mot de passe"
+                    ]
+                ],
+                'mapped' => false,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => "Votre prénom",
+                'attr' => [
+                    'placeholder' => "Indiquer votre prénom"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => "Votre nom",
+                'attr' => [
+                    'placeholder' => "Indiquer votre nom"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Valider",
+                'attr' => [
+                    'class' => "btn btn-success"
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
