9a144b8f9babbefc798bedaef6b72d6e1fd488e6
meriemrekik <rekik.meriem@gmail.com>
2024-10-05 11:39:53 +0200

    modifier le mot de passe dans BDD

---
+++ src/Form/PasswordUserType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('actuelPassword', PasswordType::class, [
+            'label' => "Votre mot de passe actuel",
+            'attr' =>['Indiquer votre mot de passe actuel'],
+            'mapped' => false 
+        ])
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'first_options' => [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => "Choisissez votre nouveau mot de passe"
+                ]
+            ],
+            'second_options' => [
+                'label' => 'Répétez votre nouveau mot de passe',
+                'attr' => [
+                    'placeholder' => "Répétez votre nouveau mot de passe"
+                ]
+            ],
+            'mapped' => false,])
+            ->add('submit', SubmitType::class, [
+                'label' => "Mettre a jour mon mot de passe",
+                'attr' => [
+                    'class' => "btn btn-success"
+                ]
+            ])
+            ->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+                $form = $event->getForm();
+                $user = $form->getConfig->getOption()['data'];
+                $passwordHasher = $form->getConfig()->getOption()['passwordHasher'];
+              
+            
+                $isValid= $passwordHasher->isPasswordValid(
+                    $user,
+                    $form->get('actualPassword')->getData()
+                );
+                if(!isValid){
+                    $form->get('actualPassword')->addError(new FormError(message:'Votre mot de passe actuel ,n\'est pas conforme.Veuillez vérifier votre saisie'));
+                }
+               
+            
+                
+            });
+            
+      
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'passwordHasher' => null
+
+        ]);
+    }
+}
